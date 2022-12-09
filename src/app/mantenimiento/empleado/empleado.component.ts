6c6c8fea35c144d5bdbe475862cd183de407295a
Othue <115855468+Othue@users.noreply.github.com>
2022-12-08 22:40:59 -0600

    Eliminar registros, tablas, grid

--- src/app/mantenimiento/empleado/empleado.component.ts
+++ src/app/mantenimiento/empleado/empleado.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/api/models';
+import { EmpleadoControllerService } from '../../api/services/empleado-controller.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
 })
-export class EmpleadoComponent {
+export class EmpleadoComponent implements OnInit{
 
+  empleado:Empleado[]=[];
+  visible:boolean=false;
+
+  constructor(
+    private empleadoService:EmpleadoControllerService,
+    private messageService:NzMessageService
+  ){}
+
+  ngOnInit(): void {
+    this.empleadoService.find().subscribe(data=>this.empleado=data)
+  }
+
+  eliminar(id:string):void{
+    this.empleadoService.deleteById({id}).subscribe(()=>{
+      this.empleado=this.empleado.filter(x => x.id !== id);
+      this.messageService.info('Registro eliminado')
+    })
+    
+  }
+
+  cancel():void{
+    this.messageService.info('Su registro sigue activo!')
+  }
+
+  ocultar():void{
+    this.visible=false
+  }
+  mostrar(data?:Empleado):void{
+    this.visible=true
+  }
+  guardar():void{
+
+  }
 }
--- src/app/mantenimiento/mantenimiento.module.ts
+++ src/app/mantenimiento/mantenimiento.module.ts
@@ -7,6 +7,15 @@ import { CargoComponent } from './cargo/cargo.component';
 import { CodigoTrabajoComponent } from './codigo-trabajo/codigo-trabajo.component';
 import { VacacionesComponent } from './vacaciones/vacaciones.component';
 
+//Importaciones de NG Zorro
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @NgModule({
   declarations: [
@@ -17,7 +26,17 @@ import { VacacionesComponent } from './vacaciones/vacaciones.component';
   ],
   imports: [
     CommonModule,
-    MantenimientoRoutingModule
+    MantenimientoRoutingModule,
+
+    NzTableModule,
+    NzDividerModule,
+    NzIconModule,
+    NzGridModule,
+    NzButtonModule,
+    NzPopconfirmModule,
+    NzMessageModule,
+    NzModalModule
+
   ]
 })
 export class MantenimientoModule { }
